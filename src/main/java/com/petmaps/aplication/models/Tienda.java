a0cffd383a8cf94fc0395e2d7b3d6599037f270e
ManuelPeregrino <manuperegrino1@gmail.com>
2022-07-26 01:55:06 -0500

    Release 1.02

--- src/main/java/com/petmaps/aplication/models/Tienda.java
+++
@@ -1,7 +0,0 @@
-package com.petmaps.aplication.models;
-
-public class Tienda extends Refugio{
-    public Tienda(String nombreRefugio, String rfc, String ubicacion, int capacidad) {
-        super(nombreRefugio, rfc, ubicacion, capacidad);
-    }
-}
